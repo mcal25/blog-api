e744c6e53b914d282cfd380b3077b05ed9104f15
mcal25 <225284010+mcal25@users.noreply.github.com>
2026-09-23 19:44:54 -0400

    Refine routing

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 import "dotenv/config";
 import express from "express";
+import { postRouter } from "./routers/postRouter";
+import { authRouter } from "./routers/authRouter";
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 
-app.use("/post")
+app.use("/post", postRouter);
+app.use("/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
---
+++ backend/routers/authRouter.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { submitSignup, submitLogin } from "../controllers/authController.js";
+
+export const authRouter = Router();
+
+authRouter.post('/signup', submitSignup)
+authRouter.post('/login', submitLogin)
