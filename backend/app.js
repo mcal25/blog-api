2aa3507c519c4f101c2bfd613e79cb8f2b2f1a50
mcal25 <225284010+mcal25@users.noreply.github.com>
2026-09-24 17:49:50 -0400

    Improve controller functionality

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+// Need this to tell express to interpret req as json so it can read 🥸
 app.use(express.json());
 app.use("/post", postRouter);
 app.use("/auth", authRouter);
--- backend/controllers/postsController.js
+++ backend/controllers/postsController.js
@@ -1,40 +1,52 @@
 import { prisma } from "../lib/prisma.js";
 
 export async function getAllPosts(req, res) {
-    const posts = await prisma.post.findMany();
-    res.json(posts);
+  const posts = await prisma.post.findMany();
+  res.json(posts);
 }
 
 export async function getPostById(req, res) {
-    const post = await prisma.post.findFirst({
-        where: {id: Number(req.params.postid)}
-    });
-    res.json(post);
+  const post = await prisma.post.findFirst({
+    where: { id: Number(req.params.postid) },
+  });
+  res.json(post);
 }
 
-export async function editPostById(req, res) {
-    console.log('request body:', req.body);
-    const post = await prisma.post.upsert({
-        where: {id: Number(req.params.postid)},
-        update: {
-            title: req.body.title,
-            body: req.body.body,
-        },
-        create: {
-            title: req.body.title,
-            body: req.body.body,
-            isPublished: false,
-            user: {
-                connect: { username: "Big Deal Randy" },
-            }
-        }
-    })
+export async function editPostById(req, res, next) {
+  console.log("request body:", req.body);
+  const post = await prisma.post.upsert({
+    where: { id: Number(req.params.postid) },
+    update: {
+      title: req.body.title,
+      body: req.body.body,
+    },
+    create: {
+      title: req.body.title,
+      body: req.body.body,
+      isPublished: false,
+      user: {
+        connect: { username: "Big Deal Randy" },
+      },
+    },
+  });
+  res.status(204).json();
 }
 
-export async function deletePostById() {
-    
+export async function deletePostById(req, res, next) {
+  const post = await prisma.post.delete({
+    where: { id: Number(req.params.postid) },
+  });
+  res.send(200).json();
 }
 
-export async function addPost() {
-    
+export async function addPost(req, res, next) {
+  const post = await prisma.post.create({
+    data: {
+      body: req.body.body,
+      title: req.body.title,
+      isPublished: false,
+      userId: 10,
+    },
+  });
+  res.send(204).json();
 }
