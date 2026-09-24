c533dcb192a979a9bc6b5b79a8d33cc4ae8d8a26
mcal25 <225284010+mcal25@users.noreply.github.com>
2026-09-24 18:29:05 -0400

    Make work update comment controller

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -1,28 +1,28 @@
 import { prisma } from "../lib/prisma.js";
 
 export async function getAllCommentsByPostId(req, res, next) {
- const comments = await prisma.comment.findMany({
-    where: { postId: Number(req.params.postid)}
- });
- res.json(comments);
+  const comments = await prisma.comment.findMany({
+    where: { postId: Number(req.params.postid) },
+  });
+  res.json(comments);
 }
 
 export async function getCommentById(req, res, next) {
- console.log(req.params);
- const comment = await prisma.comment.findFirst({
-    where: {id: Number(req.params.commentid)}
- })
- res.json(comment);
+  console.log(req.params);
+  const comment = await prisma.comment.findFirst({
+    where: { id: Number(req.params.commentid) },
+  });
+  res.json(comment);
 }
 
 export async function editCommentById(req, res, next) {
-    
+  const comment = await prisma.comment.update({
+    where: { id: Number(req.params.commentid) },
+    data: { body: req.body.body },
+  });
+  res.status(204).json();
 }
 
-export async function deleteCommentById(req, res, next) {
-    
-}
+export async function deleteCommentById(req, res, next) {}
 
-export async function addComment(req, res, next) {
-    
-}
+export async function addComment(req, res, next) {}
